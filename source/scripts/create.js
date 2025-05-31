76c97b509261bf7d8c7f12f394b223de53fa8520
Thanh-Long Nguyen-Trong <thanhlong.nguyentrong@gmail.com>
2025-05-30 23:42:24 -0700

    added autocomplete into create new memory

---
+++ source/scripts/create.js
@@ -0,0 +1,27 @@
+let autocomplete;
+
+function initAutocomplete() {
+  const input = document.getElementById('location');
+  autocomplete = new google.maps.places.Autocomplete(input, {
+    types: ['geocode'], // or use ['establishment'] or ['(regions)'] for different types of places
+    componentRestrictions: { country: 'us' }, // Restrict to US, remove if you want worldwide
+    fields: ['address_components', 'geometry', 'formatted_address'], // Limit returned data for efficiency
+  });
+
+  autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+function onPlaceChanged() {
+  const place = autocomplete.getPlace();
+
+  if (!place.geometry) {
+    // User entered something that was not suggested
+    console.log("No details available for input: '" + place.name + "'");
+    return;
+  }
+
+  console.log(place.formatted_address);
+  console.log(place.geometry.location.lat());
+  console.log(place.geometry.location.lng());
+  console.log(typeof(place))
+}
