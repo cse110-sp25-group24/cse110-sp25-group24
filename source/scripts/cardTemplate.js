3c33a564ea9c890ce8100300aa0778b4ba6b538e
widjaja <wwidjaja0@gmail.com>
2025-06-06 16:39:12 -0700

    merge with main

--- source/scripts/cardTemplate.js
+++ source/scripts/cardTemplate.js
@@ -1,5 +1,5 @@
 export function cardTemplate(data) {
-  return `
+	return `
 
   <div class="memory-card" card_id="${data.card_id}">     
       <a href="${data.link}" class="thumbnail">
@@ -141,45 +141,84 @@ export function cardTemplate(data) {
 }
 
 export const colors = {
-  nostalgic: "#ff9d24",
-  travel: "#1be287",
-  food: "#ff247d",
-  music: "#2496ff",
+	nostalgic: "#ff9d24",
+	travel: "#1be287",
+	food: "#ff247d",
+	music: "#2496ff",
 };
 
+// <memory-photo>
+//   <button
+//     onclick="// showMemoryDetails(${data.card_id})"
+//     class="memory-photo">
+//     <img id="" src="${data.img}" alt="${data.img_alt}" />
+//   </button>
+// </memory-photo>
+// <card-content>
+//   <card-meta>
+//     <!-- this should dynamically change colors -->
+//     <card-mood
+//       id="mood"
+//       moodcategory="${data.mood}"
+//       >
+//       Mood</card-mood
+//     >
+//     <card-date id="date">${data.date}</card-date>
+//     <card-actions>
+//       <button onclick="// edit in backend" id="edit-btn">✏️</button>
+//       <button onclick="// delete in backend" id="delete-btn">
+//         🗑️
+//       </button>
+//     </card-actions>
+//   </card-meta>
+//   <!-- Where would this lead? -->
+//   <a href="${data.link}" target="_blank">
+//     <h2 id="title">${data.title}</h2>
+//   </a>
+//   <p id="description">
+//     ${data.description ? data.description : "No Description Provided"}
+//   </p>
+//   <card-footer>
+//     <span id="location">📍 ${data.location ? data.location: "No Location Provided"}</span>
+//   </card-footer>
+// </card-content>
 
-            // <memory-photo>
-            //   <button
-            //     onclick="// showMemoryDetails(${data.card_id})"
-            //     class="memory-photo">
-            //     <img id="" src="${data.img}" alt="${data.img_alt}" />
-            //   </button>
-            // </memory-photo>
-            // <card-content>
-            //   <card-meta>
-            //     <!-- this should dynamically change colors -->
-            //     <card-mood
-            //       id="mood"
-            //       moodcategory="${data.mood}"
-            //       >
-            //       Mood</card-mood
-            //     >
-            //     <card-date id="date">${data.date}</card-date>
-            //     <card-actions>
-            //       <button onclick="// edit in backend" id="edit-btn">✏️</button>
-            //       <button onclick="// delete in backend" id="delete-btn">
-            //         🗑️
-            //       </button>
-            //     </card-actions>
-            //   </card-meta>
-            //   <!-- Where would this lead? -->
-            //   <a href="${data.link}" target="_blank">
-            //     <h2 id="title">${data.title}</h2>
-            //   </a>
-            //   <p id="description">
-            //     ${data.description ? data.description : "No Description Provided"}
-            //   </p>
-            //   <card-footer>
-            //     <span id="location">📍 ${data.location ? data.location: "No Location Provided"}</span>
-            //   </card-footer>
-            // </card-content>
+//   <memory-card cardid="${data.post_id}">
+//     <memory-photo>
+//       <button
+//         onclick="// showMemoryDetails(${data.post_id})"
+//         class="memory-photo"
+//       >
+//         <img id="" src="" alt="Image Not Found" />
+//       </button>
+//     </memory-photo>
+//     <card-content>
+//       <card-meta>
+//         <!-- this should dynamically change colors -->
+//         <card-mood
+//           id="mood"
+//           moodcategory="${data.mood.length > 0 ? data.mood[0] : 'none'}"
+//           ><!--${data.mood.length > 0 ? data.mood[0] : "none"}-->
+//           Mood</card-mood
+//         >
+//         <card-date id="date">${data.dateCreated}</card-date>
+//         <card-actions>
+//           <button onclick="// edit in backend" id="edit-btn">✏️</button>
+//           <button onclick="// delete in backend" id="delete-btn">
+//             🗑️
+//           </button>
+//         </card-actions>
+//       </card-meta>
+//       <!-- Where would this lead? -->
+//       <a href="${data.link}" target="_blank">
+//         <h2 id="title">${data.title}</h2>
+//       </a>
+//       <p id="description">
+//         ${data.description.length > 0 ? data.description : "No
+//         description provided."}
+//       </p>
+//       <card-footer>
+//         <span id="location">📍 ${data.location}</span>
+//       </card-footer>
+//     </card-content>
+//   </memory-card>
