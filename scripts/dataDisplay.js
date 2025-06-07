bf3e7c0a28acebf822dbd92ec3b00d3ec36f0f64
wwidjaja0 <wwidjaja0@users.noreply.github.com>
2025-06-07 08:55:29 +0000

    deploy: 01a9ea2db188d316ef345e82392bfc9ed6f75233

---
+++ scripts/dataDisplay.js
@@ -0,0 +1,114 @@
+import { cardTemplate } from "/source/scripts/cardTemplate.js";
+import { isEmptyDB } from "/source/scripts/dataHandlingFunctions.js";
+import { deleteMemory } from "/source/scripts/dataHandlingFunctions.js";
+
+// Store and Display a "Memory" using IndexedDB Issue # 30
+
+// making sure all the content is loaded before handling the DB
+window.addEventListener("DOMContentLoaded", () => {
+  const request = indexedDB.open("MemoryDB", 1); // opening DB version 1
+
+  // if database does not exist
+  request.onupgradeneeded = (event) => {
+    const db = request.result;
+
+    console.log("initializing db"); // debugging message
+
+    if (!db.objectStoreNames.contains("memories")) {
+      const store = db.createObjectStore("memories", {
+        keyPath: "post_id",
+        autoIncrement: true,
+      });
+
+      store.createIndex("dateCreated", "dateCreated", { unique: false }); // for sorting by date/getting most recent
+    }
+  };
+
+  let db; // NOTE FOR DISCUSSION: NOT PERSISTED ATM?
+
+  request.onsuccess = (event) => {
+    db = event.target.result;
+    console.log("db is up, displaying all the memories now");
+    displayAllMemories(db);
+  };
+
+  request.onerror = (event) => {
+    console.error("db err"); // works so far, seen
+  };
+});
+
+/**
+ * This function is written to display all the memories from date descending.
+ *
+ * @param {IDBDatabase} db Database instance
+ */
+function displayAllMemories(db) {
+  isEmptyDB(db).then((empty) => {
+    const display = document.querySelector("memories-grid");
+    if (empty) {
+      display.innerHTML = ``; //`<p> placeholder </p>`;
+    } else {
+      const tx = db.transaction("memories", "readonly");
+      const store = tx.objectStore("memories").index("dateCreated");
+      const request = store.openCursor(null, "prev");
+      let cnt = 0;
+      request.onsuccess = (event) => {
+        const cursor = event.target.result;
+        if (cursor) {
+          cnt += 1;
+          const post = cursor.value;
+          // console.log(cnt);
+          //create a card for the post
+          const card = document.createElement("memory-data");
+          card.setAttribute("card_id", post.post_id);
+          card.setAttribute("img", post.image);
+          card.setAttribute("img_alt", post.description || "memory image");
+          card.setAttribute("date", post.dateCreated);
+          card.setAttribute("mood", post.mood);
+          card.setAttribute("title", post.title);
+          card.setAttribute("link", post.link);
+          card.setAttribute("description", post.description);
+          card.setAttribute(
+            "location",
+            post.location || "No Location Provided",
+          );
+
+          display.appendChild(card);
+          setTimeout(() => {
+            deleteListener(card, post.post_id, db);
+          }, 0);
+          cursor.continue();
+        }
+      };
+
+      request.onerror = () => {
+        console.error("unable to open cursor");
+      };
+    }
+  });
+}
+
+/**
+ * This function adds a delete listener to the card element.
+ *
+ * @param {*} cardElement to remove from DOM
+ * @param {*} id to delete from IndexedDB
+ * @param {*} db Database instance
+ */
+function deleteListener(cardElement, id, db) {
+  const deleteBtn = cardElement.shadowRoot.querySelector("#delete-btn");
+  console.log("deleteBtn", deleteBtn);
+  if (deleteBtn) {
+    console.log("Inside Here");
+    deleteBtn.addEventListener("click", () => {
+      const confirmed = window.confirm(
+        "Are you sure you want to delete this memory?",
+      );
+      if (confirmed) {
+        deleteMemory(id, db);
+        cardElement.remove(); // Remove from DOM
+        console.log(`Deleted memory with id: ${id}`);
+      }
+    });
+  }
+}
---
+++ scripts/dataHandling.js
@@ -0,0 +1,124 @@
+import * as dhf from "./dataHandlingFunctions.js";
+import { getPlace, initCreate } from "./create.js";
+// import { getPlace, initCreate } from "./map.js"
+
+// making sure all the content is loaded before handling the DB
+window.addEventListener("DOMContentLoaded", () => {
+  const request = indexedDB.open("MemoryDB", 1); // opening DB version 1
+
+  // if database does not exist
+  request.onupgradeneeded = (event) => {
+    const db = request.result;
+
+    console.log("initializing db"); // debugging message
+
+    if (!db.objectStoreNames.contains("memories")) {
+      const store = db.createObjectStore("memories", {
+        keyPath: "post_id",
+        autoIncrement: true,
+      });
+
+      store.createIndex("dateCreated", "dateCreated", { unique: false }); // for sorting by date/getting most recent
+    }
+  };
+
+  let db;
+  // sanity checks for making sure the database is up
+
+  request.onsuccess = (event) => {
+    db = event.target.result;
+    console.log("db is up");
+    // displayLatestMemory(db); outdated 5/31/25
+  };
+
+  request.onerror = (event) => {
+    console.log("db err"); // works so far, seen
+  };
+
+  // functions to hook up to the form for testing db functions
+  /**
+    Desired Content:
+    - display the most recent memory, placeholder if nothing submitted
+
+  */
+
+  const form = document.getElementById("memory-form");
+
+  /**
+   * Memory submission creation logic.
+   */
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const data = new FormData(form);
+    const title = data.get("title");
+    const description = data.get("description");
+    const image = data.get("image");
+    const imageURL = await dhf.fileToDataUrl(image);
+    const date = new Date();
+    const locationTag = data.get("location");
+    const moodTags = data.get("mood-text");
+
+    // console.log("Location:", locationTag);
+    let place = getPlace();
+    // console.log("lat:", place.geometry.location.lat());
+    // console.log("long:", place.geometry.location.lng());
+
+    const long = place.geometry.location.lng() + Math.random() * 0.0003;
+    const lat = place.geometry.location.lat() + Math.random() * 0.0003;
+
+    const post = {
+      title: title,
+      description: description,
+      dateCreated: date,
+      image: imageURL,
+      location: locationTag,
+      longitude: long,
+      latitude: lat,
+      mood: moodTags,
+    };
+    dhf.addMemory(post, db); //.then(() => displayLatestMemory(db));
+    console.table(post); // for debugging, post data is displayed in
+    event.target.reset();
+    // addMarker(window.map, locationTag.geometry.location.lat(), locationTag.geometry.location.lng(), title);
+  });
+
+  const imageInput = document.getElementById("imageUpload");
+  const imagePreview = document.getElementById("imagePreview");
+
+  imageInput.addEventListener("change", () => {
+    const files = imageInput.files;
+
+    if (files.length > 1) {
+      alert("Please select only one image file.");
+      imageInput.value = "";
+      imagePreview.src = "";
+      return;
+    }
+
+    const file = files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      imagePreview.src = reader.result;
+    };
+    reader.readAsDataURL(file);
+  });
+
+  // Load data from local storage
+  const userData = JSON.parse(localStorage.getItem("userData"));
+
+  if (userData) {
+    document.getElementById("imagePreview").src = userData.imgSrc;
+    document.getElementById("title").value = userData.titleText;
+    document.getElementById("description").value = userData.descriptionText;
+    document.getElementById("mood-text").value = userData.mood;
+    // document.getElementById("music").value = userData.music;
+    // alert(userData.imgSrc);
+
+    // Delete local storage after use
+    localStorage.removeItem("userData");
+  }
+
+  initCreate();
+});
